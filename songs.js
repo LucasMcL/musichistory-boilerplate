427130fdc805234f381b31c142e28576d8fe624c
LucasMcL <LMcLaughlin09@gmail.com>
2016-12-04 10:40:31 -0600

    added song to beggining and end of song array.  use console.log to output for testing

--- songs.js
+++ songs.js
@@ -1,3 +1,20 @@
+// Use JavaScript arrays, loops, and innerHTML to show
+// the music you love.
+
+// Students must use JavaScript to create a list of
+// songs in the index.html file for their Music History project.
+// Have them download the songs.js file,
+// which contains an array of strings with song information.
+
+// Each student must add one song to the beginning and the
+// end of the array.
+// Loop over the array and remove any words or characters
+// that obviously don't belong.
+// Students must find and replace the > character in
+// each item with a - character.
+// Must add each string to the DOM in index.html in
+// the main content area.
+
 var songs = [];
 
 songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
@@ -5,3 +22,10 @@ songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast 
 songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+
+console.log(songs);
+
+songs.unshift("Elephant in the Dock - by mewithoutYou on the album Ten Stories")
+songs.push("Fourth of July - by Sufjan Stevens on the album Carrie and Lowell")
+
+console.log(songs);
