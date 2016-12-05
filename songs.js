c3f1ad7abffac4ae6b30e5eb3f79e5fd8bfaef9e
LucasMcL <LMcLaughlin09@gmail.com>
2016-12-04 19:42:43 -0600

    cleaned data and dynamically generated html in main panel with song, album, and artist info

--- songs.js
+++ songs.js
@@ -1,19 +1,14 @@
-// Use JavaScript arrays, loops, and innerHTML to show
-// the music you love.
+//////////////////////////////////////////////////////
+/////////           OBJECTIVES             ///////////
+//////////////////////////////////////////////////////
 
-// Students must use JavaScript to create a list of
-// songs in the index.html file for their Music History project.
-// Have them download the songs.js file,
-// which contains an array of strings with song information.
+// Use regular expressions to replace bad characters
+// Use regular expressions to replace > with -
+// Add song to beggining and end
+// Link to main content area
+  // Loop through array, pull out artist, song, and album info
+  // Dynamically generate html from that
 
-// Each student must add one song to the beginning and the
-// end of the array.
-// Loop over the array and remove any words or characters
-// that obviously don't belong.
-// Students must find and replace the > character in
-// each item with a - character.
-// Must add each string to the DOM in index.html in
-// the main content area.
 
 var songs = [];
 
@@ -23,9 +18,65 @@ songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album Th
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
-console.log(songs);
+// Remove junk characters and replace > with -
+for(var i = 0; i < songs.length; i++) {
+  // The regular expression matches any number of instances of the
+  // characters *, @, (, or ! and replaces them with empty strings
+  songs[i] = songs[i].replace(/\*|@|\(|!/g, "");
+  songs[i] = songs[i].replace(/>/, "-");
+}
 
+// Add songs to beginning and end
 songs.unshift("Elephant in the Dock - by mewithoutYou on the album Ten Stories")
 songs.push("Fourth of July - by Sufjan Stevens on the album Carrie and Lowell")
 
-console.log(songs);
+// Extract info from array
+var re = /(.+)\s-\sby\s(.+)\son\sthe\salbum\s(.+)/i
+var data = []
+for(var i = 0; i < songs.length; i++) {
+  var matches = songs[i].match(re);
+  //Creates array of objects
+  data.push({
+    song: matches[1],
+    artist: matches[2],
+    album: matches[3]
+    })
+}
+
+var mainPanelEl = document.getElementById('main-panel')
+// <h1>Song Name</h1><p><span>Artist</span> |
+// <span>Album</span> | <span>Genre</span></p>
+
+
+for (var i = 0; i < data.length; i++) {
+  var h2 = document.createElement("h2")
+  var h2Text = document.createTextNode(data[i].song)
+  h2.appendChild(h2Text)
+  mainPanelEl.appendChild(h2)
+  var p = document.createElement("p")
+  mainPanelEl.appendChild(p)
+  p.innerHTML = "<span>" + data[i].artist + "</span> | " +
+                "<span>" + data[i].album + "</span> | " +
+                "<span>" + "Genre" + "</span>";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
