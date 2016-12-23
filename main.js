3e16717c6eb51ca0dfd5956040ffc509636ec74a
LucasMcL <LMcLaughlin09@gmail.com>
2016-12-23 12:31:38 -0600

    more songs, add songs, and delete buttons all working

--- main.js
+++ main.js
@@ -1,50 +1,9 @@
-// To do: disable more songs button after clicked
-  // Delete buttons working
+// To do:
 
-// This jQuery function gets the JSON data and populates the page
-// Does not store the data in a global variable
-var listCounter = 0
-$.getJSON( "data/songs.json", function(songs) {
-  for (var i = 0; i < songs.data.length; i++) {
-    var div = $(`<div id=${listCounter}></div>`)
-    div.append(`<h2>${songs.data[i].song}</h2>`)
-    div.append(`<span>${songs.data[i].artist}</span>`)
-    div.append('<span>|</span>')
-    div.append(`<span>${songs.data[i].album}</span>`)
-    div.append('<span>|</span>')
-    div.append(`<span>Genre</span>`)
-    div.append('<input type="button" value="delete" name="Delete" class="delete-btn hidden">')
 
-    $('#main-panel').append(div)
-
-    listCounter++
-
-  }
-  // Create more songs button and add event listener to it
-  $('#main-panel').append('<input type="button" value="more-songs" name="More songs" id="more-songs-btn">')
-  $('#more-songs-btn').click( addMoreSongs )
-})
-
-function addMoreSongs() {
-  $.getJSON('data/more-songs.json', function(songs) {
-    for (var i = 0; i < songs.data.length; i++) {
-      var div = $(`<div id=${listCounter}></div>`)
-      div.append(`<h2>${songs.data[i].song}</h2>`)
-      div.append(`<span>${songs.data[i].artist}</span>`)
-      div.append('<span>|</span>')
-      div.append(`<span>${songs.data[i].album}</span>`)
-      div.append('<span>|</span>')
-      div.append(`<span>Genre</span>`)
-      div.append('<input type="button" value="delete" name="Delete" class="delete-btn hidden">')
-
-      div.insertAfter($('#main-panel div').last())
-
-      listCounter++
-    }
-  })
-  $('#more-songs-btn').attr('disabled', 'disabled')
-}
-// Add event listeners to navbar links
+/////////////////////////////////////
+////////SPA Functionality////////////
+/////////////////////////////////////
 
 // Changes which sections are shown/hidden
 // Changes which navbar link is targeted w/class viewing
@@ -66,15 +25,73 @@ $('#link__add-music').click( function() {
   $('#link__add-music').addClass('viewing')
 })
 
+/////////////////////////////////////
+////////Load Initial Songs///////////
+/////////////////////////////////////
+
+// This jQuery function gets the JSON data and populates the page
+// Does not store the data in a global variable
+var listCounter = 0
+$.getJSON( "data/songs.json", function(songs) {
+  for (var i = 0; i < songs.data.length; i++) {
+    var div = $(`<div id=${listCounter}></div>`)
+    div.append(`<h2>${songs.data[i].song}</h2>`)
+    div.append(`<span>${songs.data[i].artist}</span>`)
+    div.append('<span>|</span>')
+    div.append(`<span>${songs.data[i].album}</span>`)
+    div.append('<span>|</span>')
+    div.append(`<span>Genre</span>`)
+    div.append('<input type="button" value="delete" name="Delete" class="delete-btn hidden">')
+
+    $('#main-panel').append(div)
+
+    listCounter++
+
+    activateDeleteButton($('#main-panel div').last())
+  }
+  // Create more songs button and add event listener to it
+  $('#main-panel').append('<input type="button" value="more-songs" name="More songs" id="more-songs-btn">')
+  $('#more-songs-btn').click( addMoreSongs )
+})
+
+/////////////////////////////////////
+//////////Add More Songs/////////////
+/////////////////////////////////////
+
+function addMoreSongs() {
+  $.getJSON('data/more-songs.json', function(songs) {
+    for (var i = 0; i < songs.data.length; i++) {
+      var div = $(`<div id=${listCounter}></div>`)
+      div.append(`<h2>${songs.data[i].song}</h2>`)
+      div.append(`<span>${songs.data[i].artist}</span>`)
+      div.append('<span>|</span>')
+      div.append(`<span>${songs.data[i].album}</span>`)
+      div.append('<span>|</span>')
+      div.append(`<span>Genre</span>`)
+      div.append('<input type="button" value="delete" name="Delete" class="delete-btn hidden">')
+
+      div.insertAfter($('#main-panel div').last())
+
+      listCounter++
+
+      activateDeleteButton($('#main-panel div').last())
+    }
+  })
+  $('#more-songs-btn').attr('disabled', 'disabled')
+}
+
+/////////////////////////////////////
+////////    Add Songs    ////////////
+/////////////////////////////////////
+
 // When the add button is clicked
 // Create new object containing song, artist, and album info
-// Push new object to array of objects
 $('input[name=add]').click( function() {
+  // Could add this object to global data variable if we had one
   var newInfo = {}
   newInfo.song = $('input[name=song]').val()
   newInfo.artist = $('input[name=artist]').val()
   newInfo.album = $('input[name=album]').val()
-  data.push(newInfo)
 
   // Clear input fields
   $('input[name=song]').val('')
@@ -93,32 +110,26 @@ $('input[name=add]').click( function() {
 
   listCounter++
 
-  // Insert div into DOM
-  $('#main-panel').append(div)
+  // Insert div into DOM after last div
+  div.insertAfter($('#main-panel div').last())
 
   // Add event listeners to just added div
-  $('#main-panel div').last().mouseover( function() {
-    $(this).find('.delete-btn').removeClass('hidden')
+  activateDeleteButton($('#main-panel div').last())
+})
+
+// Function to activate a single delete button
+// Takes a jQuery element as argument
+function activateDeleteButton(jqObj) {
+  jqObj.mouseover( function() {
+    jqObj.find('.delete-btn').removeClass('hidden')
   })
-
-  $('#main-panel div').last().mouseleave( function() {
-    $(this).find('.delete-btn').addClass('hidden')
+  jqObj.mouseleave( function() {
+    jqObj.find('.delete-btn').addClass('hidden')
   })
-})
-
-// Delete buttons appear on hover
-$('.main-panel div').mouseover( function() {
-  $(this).find('.delete-btn').removeClass('hidden')
-})
-
-$('.main-panel div').mouseleave( function() {
-  $(this).find('.delete-btn').addClass('hidden')
-})
-
-// Removes parent div when delete button is clicked
-$('.main-panel .delete-btn').click( function(clickEvt) {
-  $(clickEvt.target.parentNode).remove()
-})
+  jqObj.find('.delete-btn').click( function(clickEvt) {
+    $(clickEvt.target.parentNode).remove()
+  })
+}
 
 
 
